c3d18b219110580d97721f74ad075b5b6927f4a0
vitalii-chesnokov <153494158+vitalii-chesnokov@users.noreply.github.com>
2024-03-25 11:45:21 +0200

    Update R_ja_a.py

--- R ja a/R_ja_a.py
+++ R ja a/R_ja_a.py
@@ -1,27 +1,27 @@
 from MinuOmaMoodul import*
-salasõnad=loe_failist(Paroolid.txt)
-kasutajanimed=loe_failist(Nimed.txt)
+salasõnad=loe_failist("Paroolid.txt")
+kasutajanimed=loe_failist("Nimed.txt")
 while True:
-    print(Kasutajanimed)
+    print(kasutajanimed)
     print(salasõnad)
     print("1-registeerimine\n2-autoriseerimine\n3-nime või parooli muutmine\n4-unustanud parooli taastamine\n5-lõpetamine")
-    vastus=input("Sisestage arv")
+    vastus=int(input("Sisestage arv"))
     if vastus == 1:
         print("Registeerimine")
-        kasutajanimed,salasõnad=registreerimine(kasutajanimed,salasõnad)
+        kasutajanimed,salasõnad=registeerimine(kasutajanimed,salasõnad)
     elif vastus == 2:
         print("Autoriseerimine")
-        autoriseerimine(kasutajad,salasõna)
+        autoriseerimine(kasutajanimed,salasõnad)
     elif vastus == 3 :
         print("Nimi  või parooli muutmine")
         vastus==int(input("Kas muudame nimi või parooli?"))
         if vastus == "nimi":
             kasutajanimed=muutmine(kasutajanimed)
         elif vastus == "parool":
-            salasõma=muutmine(salasõna)
+            salasõma=muutmine(salasõnad)
         elif vastus == "mõlemad":
             kasutajanimed=muutmine(kasutajanimed)
-            salasõma=muutmine(salasõna)
+            salasõma=muutmine(salasõnad)
     elif vastus == 4:
         print("Unustanud porooli taastamine")
     elif vastus == 5:
